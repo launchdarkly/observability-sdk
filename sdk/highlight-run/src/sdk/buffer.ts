dd86bab2c1bf5aff797a4cc05aeff67d1af70246
Vadim Korolik <vkorolik@gmail.com>
2025-05-09 12:58:36 -0500

    enforce max size for call buffer

--- sdk/highlight-run/src/sdk/buffer.ts
+++ sdk/highlight-run/src/sdk/buffer.ts
@@ -1,7 +1,15 @@
+import { Logger } from '../client/logger'
+
+type Event = { method: string; args: any[] }
+
 export class BufferedClass<T extends object> {
 	protected _sdk!: T
 	protected _isLoaded = false
-	protected _callBuffer: Array<{ method: string; args: any[] }> = []
+	protected _callBuffer: Array<Event> = []
+	protected _capacity: number = 100_000
+	protected _droppedEvents: number = 0
+	protected _exceededCapacity: boolean = false
+	protected _logger = new Logger()
 
 	protected _bufferCall(method: string, args: any[]) {
 		if (this._isLoaded) {
@@ -9,8 +17,23 @@ export class BufferedClass<T extends object> {
 			return (this._sdk as any)[method](...args)
 		} else {
 			// Otherwise buffer the call
-			this._callBuffer.push({ method, args })
+			this._enqueue({ method, args })
 			return undefined
+		}
+	}
+
+	protected _enqueue(event: Event) {
+		if (this._callBuffer.length < this._capacity) {
+			this._callBuffer.push(event)
+			this._exceededCapacity = false
+		} else {
+			if (!this._exceededCapacity) {
+				this._exceededCapacity = true
+				this._logger.warn(
+					'Exceeded event queue capacity. Increase capacity to avoid dropping events.',
+				)
+			}
+			this._droppedEvents += 1
 		}
 	}
 
