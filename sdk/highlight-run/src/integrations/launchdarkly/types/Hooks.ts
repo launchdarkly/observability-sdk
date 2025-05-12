4e4104a6025a003383ab755dd89f532c62941b4c
Vadim Korolik <vkorolik@gmail.com>
2025-05-12 16:29:23 -0500

    fixup! fix types to be compatible between ld 3.x and 4.x

--- sdk/highlight-run/src/integrations/launchdarkly/types/Hooks.ts
+++ sdk/highlight-run/src/integrations/launchdarkly/types/Hooks.ts
@@ -1,44 +1,15 @@
 import type {
-	IdentifySeriesContext,
-	IdentifySeriesData,
-	IdentifySeriesResult,
 	EvaluationSeriesContext,
 	EvaluationSeriesData,
-	HookMetadata,
-	TrackSeriesContext,
 	LDEvaluationReason,
 	LDFlagValue,
+	Hook as LDHook,
 } from '@launchdarkly/js-client-sdk'
 
 /**
  * Interface for extending SDK functionality via hooks.
  */
-export interface Hook {
-	/**
-	 * Get metadata about the hook implementation.
-	 */
-	getMetadata(): HookMetadata
-
-	/**
-	 * This method is called during the execution of a variation method
-	 * before the flag value has been determined. The method is executed synchronously.
-	 *
-	 * @param hookContext Contains information about the evaluation being performed. This is not
-	 *  mutable.
-	 * @param data A record associated with each stage of hook invocations. Each stage is called with
-	 * the data of the previous stage for a series. The input record should not be modified.
-	 * @returns Data to use when executing the next state of the hook in the evaluation series. It is
-	 * recommended to expand the previous input into the return. This helps ensure your stage remains
-	 * compatible moving forward as more stages are added.
-	 * ```js
-	 * return {...data, "my-new-field": /*my data/*}
-	 * ```
-	 */
-	beforeEvaluation?(
-		hookContext: EvaluationSeriesContext,
-		data: EvaluationSeriesData,
-	): EvaluationSeriesData
-
+export type Hook = Omit<LDHook, 'afterEvaluation'> & {
 	/**
 	 * This method is called during the execution of the variation method
 	 * after the flag value has been determined. The method is executed synchronously.
@@ -73,57 +44,7 @@ export interface Hook {
 			/**
 			 * An object describing the main factor that influenced the flag evaluation value.
 			 */
-			reason?: LDEvaluationReason
+			reason?: LDEvaluationReason | null
 		},
 	): EvaluationSeriesData
-
-	/**
-	 * This method is called during the execution of the identify process before the operation
-	 * completes, but after any context modifications are performed.
-	 *
-	 * @param hookContext Contains information about the evaluation being performed. This is not
-	 *  mutable.
-	 * @param data A record associated with each stage of hook invocations. Each stage is called with
-	 * the data of the previous stage for a series. The input record should not be modified.
-	 * @returns Data to use when executing the next state of the hook in the evaluation series. It is
-	 * recommended to expand the previous input into the return. This helps ensure your stage remains
-	 * compatible moving forward as more stages are added.
-	 * ```js
-	 * return {...data, "my-new-field": /*my data/*}
-	 * ```
-	 */
-	beforeIdentify?(
-		hookContext: IdentifySeriesContext,
-		data: IdentifySeriesData,
-	): IdentifySeriesData
-
-	/**
-	 * This method is called during the execution of the identify process before the operation
-	 * completes, but after any context modifications are performed.
-	 *
-	 * @param hookContext Contains information about the evaluation being performed. This is not
-	 *  mutable.
-	 * @param data A record associated with each stage of hook invocations. Each stage is called with
-	 * the data of the previous stage for a series. The input record should not be modified.
-	 * @returns Data to use when executing the next state of the hook in the evaluation series. It is
-	 * recommended to expand the previous input into the return. This helps ensure your stage remains
-	 * compatible moving forward as more stages are added.
-	 * ```js
-	 * return {...data, "my-new-field": /*my data/*}
-	 * ```
-	 */
-	afterIdentify?(
-		hookContext: IdentifySeriesContext,
-		data: IdentifySeriesData,
-		result: IdentifySeriesResult,
-	): IdentifySeriesData
-
-	/**
-	 * This method is called during the execution of the track process after the event
-	 * has been enqueued.
-	 *
-	 * @param hookContext Contains information about the track operation being performed. This is not
-	 *  mutable.
-	 */
-	afterTrack?(hookContext: TrackSeriesContext): void
 }
