4e0acc4d47169a6965b075f446f4bf557f449f6f
Vadim Korolik <vkorolik@gmail.com>
2025-06-06 00:10:25 -0500

    fix e2e react-router app

--- e2e/react-router/src/ldclient.tsx
+++ e2e/react-router/src/ldclient.tsx
@@ -3,7 +3,25 @@ import { initialize as init } from 'launchdarkly-js-client-sdk'
 import Observability from '@launchdarkly/observability'
 import SessionReplay from '@launchdarkly/session-replay'
 // import { LD } from '@launchdarkly/browser'
-// import { withLDProvider } from 'launchdarkly-react-client-sdk'
+import { withLDProvider } from 'launchdarkly-react-client-sdk'
+
+const observabilitySettings: ConstructorParameters<typeof Observability>[0] = {
+	networkRecording: {
+		enabled: true,
+		recordHeadersAndBody: true,
+	},
+	serviceName: 'ryan-test',
+	backendUrl: 'https://pub.observability.ld-stg.launchdarkly.com',
+	otel: {
+		otlpEndpoint: 'https://otel.observability.ld-stg.launchdarkly.com',
+	},
+}
+const sessionReplaySettings: ConstructorParameters<typeof SessionReplay>[0] = {
+	debug: { clientInteractions: true, domRecording: true },
+	privacySetting: 'none',
+	serviceName: 'ryan-test',
+	backendUrl: 'https://pub.observability.ld-stg.launchdarkly.com',
+}
 
 export const client = init(
 	'66d9d3c255856f0fa8fd62d0',
@@ -11,41 +29,19 @@ export const client = init(
 	{
 		// Not including plugins at all would be equivalent to the current LaunchDarkly SDK.
 		plugins: [
-			new Observability({
-				networkRecording: {
-					enabled: true,
-					recordHeadersAndBody: true,
-				},
-				serviceName: 'ryan-test',
-				backendUrl: 'https://pub.observability.ld-stg.launchdarkly.com',
-				otel: {
-					otlpEndpoint:
-						'https://otel.observability.ld-stg.launchdarkly.com',
-				},
-			}),
-			new SessionReplay({
-				debug: { clientInteractions: true, domRecording: true },
-				privacySetting: 'none',
-				serviceName: 'ryan-test',
-				backendUrl: 'https://pub.observability.ld-stg.launchdarkly.com',
-			}), // Could be omitted for customers who cannot use session replay.
+			new Observability(observabilitySettings),
+			new SessionReplay(sessionReplaySettings),
 		],
 	},
 )
 
-/*export const LDProvider = withLDProvider({
-	// ... all existing options
+export const LDProvider = withLDProvider({
+	clientSideID: '66d9d3c255856f0fa8fd62d0',
+	context: { key: 'unknown' },
 	options: {
 		plugins: [
-			new Observability('1', {
-				networkRecording: {
-					enabled: true,
-					recordHeadersAndBody: true,
-				},
-			}),
-			new SessionReplay('1', {
-				privacySetting: 'none',
-			}),
+			new Observability(observabilitySettings),
+			new SessionReplay(sessionReplaySettings),
 		],
 	},
-})*/
+})
