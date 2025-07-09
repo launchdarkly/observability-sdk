8ecf9de483b147e8cbe4e13bfa5c16d5d641694f
ccschmitz <ccschmitz@gmail.com>
2025-07-09 13:54:22 -0500

    Update index.ts

--- sdk/@launchdarkly/observability-shared/src/index.ts
+++ sdk/@launchdarkly/observability-shared/src/index.ts
@@ -1,4 +1,4 @@
 export { RECORD_ATTRIBUTE } from './instrumentation/constants'
 export { CustomTraceContextPropagator } from './instrumentation/propagator'
-export { TracingOrigins } from './instrumentation/types'
+export type { TracingOrigins } from './instrumentation/types'
 export { getCorsUrlsPattern, getSpanName } from './instrumentation/utils'
