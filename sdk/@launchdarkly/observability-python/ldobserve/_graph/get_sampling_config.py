04459bd78674984b0ce6354c80cdd5b00db2c350
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-07-09 16:25:08 -0700

    Additional error handling for getting sampling config.

--- sdk/@launchdarkly/observability-python/ldobserve/_graph/get_sampling_config.py
+++ sdk/@launchdarkly/observability-python/ldobserve/_graph/get_sampling_config.py
@@ -20,24 +20,36 @@ def get_sampling_config(
     client = Client(url=endpoint)
 
     async def _get_sampling_config():
-        result = await client.get_sampling_config(project_id)
-        logging.getLogger(__name__).debug("Got sampling config: %s", result)
-        callback(result.sampling)
+        try:
+            result = await client.get_sampling_config(project_id)
+            logging.getLogger(__name__).debug("Got sampling config: %s", result)
+            callback(result.sampling)
+        except Exception as e:
+            logging.getLogger(__name__).error("Error getting sampling config: %s", e)
+
+    # The graphql client uses asyncio internally, so we need to be able to execute async code.
+    # Some async environments disallow creating threads, so we check to see if we are already in an async context
+    # and if we are, we run the task in the existing event loop.
+    # Otherwise, we create a thread and run the task in it.
 
     is_async = False
     try:
         # Get the running event loop to check if we're in an async context.
         # If we are not, then this will raise a RuntimeError.
         asyncio.get_running_loop()
-        task = asyncio.create_task(_get_sampling_config())
-        tasks.add(task)
-        task.add_done_callback(lambda task: tasks.remove(task))
         is_async = True
-        logging.getLogger(__name__).debug("Running in existing asyncio event loop.")
     except RuntimeError:
         pass
 
-    if not is_async:
+    if is_async:
+        logging.getLogger(__name__).debug("Running in existing asyncio event loop.")
+        task = asyncio.create_task(_get_sampling_config())
+        # The executor only keeps a weak reference to the task, so we need to keep a strong reference to it.
+        # If we don't then it could get collected while it is still running.
+        tasks.add(task)
+        # We need to remove the task when it is done so it can be garbage collected.
+        task.add_done_callback(lambda task: tasks.remove(task))
+    else:
         # We were not in an async context, so we need to run the task in a thread.
         logging.getLogger(__name__).debug(
             "No running event loop found, running in thread."
