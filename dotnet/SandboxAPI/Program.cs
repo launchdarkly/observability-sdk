7f90984cfbb37f054507a841e310753b42d58df7
Robert Pakko <rpakko@launchdarkly.com>
2025-08-04 11:49:18 -0700

    Sample app which fetches sdk flag

--- dotnet/SandboxAPI/Program.cs
+++ dotnet/SandboxAPI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.OpenApi.Models;
+using LaunchDarkly.Sdk;
+using LaunchDarkly.Sdk.Server;
 
+// DOTNET Setup
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -18,6 +21,38 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+// LaunchDarkly SDK Setup
+string SdkKey = Environment.GetEnvironmentVariable("LAUNCHDARKLY_SDK_KEY");
+
+if (string.IsNullOrEmpty(SdkKey))
+{
+    Console.WriteLine("*** Please set LAUNCHDARKLY_SDK_KEY environment variable to your LaunchDarkly SDK key first\n");
+    Environment.Exit(1);
+}
+
+var ldConfig = Configuration.Default(SdkKey);
+
+var client = new LdClient(ldConfig);
+
+if (client.Initialized)
+{
+    Console.WriteLine("*** SDK successfully initialized!\n");
+}
+else
+{
+    Console.WriteLine("*** SDK failed to initialize\n");
+    Environment.Exit(1);
+}
+
+var context = Context.Builder("context-key-123abc")
+    .Name("Robert Pakko")
+    .Build();
+
+// API Endpoints
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/featureFlag", () => {
+    var featureFlag = client.BoolVariation("sample-flag", context, false);
+    return Results.Ok(featureFlag);
+});
 
 app.Run();
