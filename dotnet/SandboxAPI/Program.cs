a788fdc9dd712401975862baabe10f2d81e573ec
Robert Pakko <rpakko@launchdarkly.com>
2025-08-04 09:47:02 -0700

    Created sandbox API

---
+++ dotnet/SandboxAPI/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sandbox API", Description = "This is a sandbox API for dotnet SDK development", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sandbox API V1");
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
