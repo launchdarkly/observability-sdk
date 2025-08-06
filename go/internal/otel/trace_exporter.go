da8d65eea2cfeee325898880986abb91108d5874
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-08-06 15:39:07 -0700

    feat: Sample out children of spans that have been sampled out.

--- go/internal/otel/trace_exporter.go
+++ go/internal/otel/trace_exporter.go
@@ -4,11 +4,12 @@ import (
 	"context"
 
 	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/sdk/trace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type traceExporter struct {
-	trace.SpanExporter
+	sdktrace.SpanExporter
 	sampler ExportSampler
 }
 
@@ -18,7 +19,7 @@ type traceExporter struct {
 // So we wrap the ReadOnlySpan in a struct and provide an alternate implementation
 // of the Attributes method that returns the original attributes plus the extra attributes.
 type readOnlySpanWorkaround struct {
-	trace.ReadOnlySpan
+	sdktrace.ReadOnlySpan
 	extraAttributes []attribute.KeyValue
 }
 
@@ -27,9 +28,51 @@ func (r readOnlySpanWorkaround) Attributes() []attribute.KeyValue {
 }
 
 // ExportSpans implements trace.SpanExporter.
-func (t *traceExporter) ExportSpans(ctx context.Context, spans []trace.ReadOnlySpan) error {
-	exportedSpans := make([]trace.ReadOnlySpan, 0, len(spans))
+func (t *traceExporter) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan) error {
+	omittedSpanIds := make([]trace.SpanID, 0, len(spans))
+	spanById := make(map[trace.SpanID]sdktrace.ReadOnlySpan)
+	childrenByParentId := make(map[trace.SpanID][]trace.SpanID)
+
+	// THe first pass we sample items which are directly impacted by a sampling decision.
+	// We also build a map of children spans by parent span id, which allows us to quickly traverse the span tree.
 	for _, s := range spans {
+		parentId := s.Parent().SpanID()
+		if parentId.IsValid() {
+			childrenByParentId[parentId] = append(childrenByParentId[parentId], s.SpanContext().SpanID())
+		}
+
+		res := t.sampler.SampleSpan(s)
+		if res.Sample {
+			rs := readOnlySpanWorkaround{
+				ReadOnlySpan:    s,
+				extraAttributes: res.Attributes,
+			}
+			spanById[s.SpanContext().SpanID()] = rs
+		} else {
+			omittedSpanIds = append(omittedSpanIds, s.SpanContext().SpanID())
+		}
+	}
+
+	// Find all children of spans that have been sampled out and remove them.
+	// Repeat until there are no more children to remove.
+	for len(omittedSpanIds) != 0 {
+
+		spanId, o := omittedSpanIds[0], omittedSpanIds[1:]
+		omittedSpanIds = o
+
+		affectedSpans := childrenByParentId[spanId]
+		if affectedSpans == nil {
+			continue
+		}
+		omittedSpanIds = append(omittedSpanIds, affectedSpans...)
+
+		for _, affectedSpanId := range affectedSpans {
+			delete(spanById, affectedSpanId)
+		}
+	}
+
+	exportedSpans := make([]sdktrace.ReadOnlySpan, 0, len(spans))
+	for _, s := range spanById {
 		res := t.sampler.SampleSpan(s)
 		if res.Sample {
 			exportedSpans = append(
@@ -45,8 +88,8 @@ func (t *traceExporter) ExportSpans(ctx context.Context, spans []trace.ReadOnlyS
 	return t.SpanExporter.ExportSpans(ctx, exportedSpans)
 }
 
-func newTraceExporter(exporter trace.SpanExporter, sampler ExportSampler) *traceExporter {
+func newTraceExporter(exporter sdktrace.SpanExporter, sampler ExportSampler) *traceExporter {
 	return &traceExporter{SpanExporter: exporter, sampler: sampler}
 }
 
-var _ trace.SpanExporter = &traceExporter{}
+var _ sdktrace.SpanExporter = &traceExporter{}
