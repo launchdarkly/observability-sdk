4a1b481c267c894741a546e7e312944e6e4bdd64
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-08-11 10:57:50 -0700

    feat: Add trace based sampling for go plugin.

--- go/config.go
+++ go/config.go
@@ -3,18 +3,21 @@ package ldobserve
 import (
 	"context"
 
+	"go.opentelemetry.io/otel/trace"
+
 	"github.com/launchdarkly/observability-sdk/go/internal/defaults"
 )
 
 type observabilityConfig struct {
-	serviceName    string
-	serviceVersion string
-	environment    string
-	backendURL     string
-	otlpEndpoint   string
-	manualStart    bool
-	context        context.Context
-	debug          bool
+	serviceName     string
+	serviceVersion  string
+	environment     string
+	backendURL      string
+	otlpEndpoint    string
+	manualStart     bool
+	context         context.Context
+	debug           bool
+	samplingRateMap map[trace.SpanKind]float64
 }
 
 func defaultConfig() observabilityConfig {
@@ -85,4 +88,13 @@ func WithDebug() Option {
 	return func(c *observabilityConfig) {
 		c.debug = true
 	}
+}
+
+// WithSamplingRateMap sets the sampling rate for each span kind.
+// This setting can influence the quality of metrics used for experiments and guarded
+// releases and should only be adjusted with consultation.
+func WithSamplingRateMap(rates map[trace.SpanKind]float64) Option {
+	return Option(func(conf *observabilityConfig) {
+		conf.samplingRateMap = rates
+	})
 }
