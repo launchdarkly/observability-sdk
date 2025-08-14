c63315bed27c463a829794bb740fa3e316cae0c2
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-08-14 15:58:17 -0700

    De-duplicate activity creation.

--- sdk/@launchdarkly/observability-dotnet/test/LaunchDarkly.Observability.Tests/SampleSpansTests.cs
+++ sdk/@launchdarkly/observability-dotnet/test/LaunchDarkly.Observability.Tests/SampleSpansTests.cs
@@ -11,104 +11,27 @@ namespace LaunchDarkly.Observability.Test
     [TestFixture]
     public class SampleSpansTests
     {
-        #region Helper Classes and Methods
-
-        /// <summary>
-        /// Mock implementation of IExportSampler for testing
-        /// </summary>
-        private class MockSampler : IExportSampler
-        {
-            private readonly Dictionary<string, bool> _mockResults;
-            private readonly bool _enabled;
-
-            public MockSampler(Dictionary<string, bool> mockResults, bool enabled = true)
-            {
-                _mockResults = mockResults;
-                _enabled = enabled;
-            }
-
-            public void SetConfig(SamplingConfig config)
-            {
-                // Not needed for tests
-            }
-
-            public SamplingResult SampleSpan(Activity span)
-            {
-                var spanId = span.SpanId.ToString();
-                var shouldSample = _mockResults.GetValueOrDefault(spanId, true);
-
-                return new SamplingResult
-                {
-                    Sample = shouldSample,
-                    Attributes = shouldSample
-                        ? new Dictionary<string, object> { ["samplingRatio"] = 2 }
-                        : new Dictionary<string, object>()
-                };
-            }
-
-            public SamplingResult SampleLog(LogRecord record)
-            {
-                return new SamplingResult { Sample = true };
-            }
-
-            public bool IsSamplingEnabled()
-            {
-                return _enabled;
-            }
-        }
-
-        /// <summary>
-        /// Helper method to create a mock Activity
-        /// </summary>
-        private static Activity CreateMockActivity(string name, string parentSpanId = null)
-        {
-            var activity = new Activity(name);
-            activity.Start();
-            activity.DisplayName = name;
-
-            // Set a custom span ID for predictable testing
-            activity.SetIdFormat(ActivityIdFormat.W3C);
-
-            // If we have a parent span ID, we need to create a proper parent context
-            if (!string.IsNullOrEmpty(parentSpanId))
-            {
-                // Create a trace ID and parent span context
-                var traceId = ActivityTraceId.CreateRandom();
-                var parentSpan = ActivitySpanId.CreateFromString(parentSpanId.PadRight(16, '0'));
-                var parentContext = new ActivityContext(traceId, parentSpan, ActivityTraceFlags.Recorded);
-
-                // Stop and recreate the activity with the parent context
-                activity.Stop();
-                activity = new Activity(name);
-                activity.SetParentId(parentContext.TraceId, parentContext.SpanId, parentContext.TraceFlags);
-                activity.Start();
-                activity.DisplayName = name;
-            }
-
-            activity.Stop();
-            return activity;
-        }
-
-        #endregion
-
         [Test]
         public void SampleActivities_ShouldRemoveSpansThatAreNotSampled()
         {
             var activities = new List<Activity>
             {
-                CreateMockActivity("span-1"), // Root span - sampled
-                CreateMockActivity("span-2") // Root span - not sampled
+                TestActivityHelper.CreateTestActivity("span-1"), // Root span - sampled
+                TestActivityHelper.CreateTestActivity("span-2") // Root span - not sampled
             };
 
             var span1 = activities[0];
             var span2 = activities[1];
 
             // We need to mock the span IDs in our sampler
-            var samplerWithRealIds = new MockSampler(new Dictionary<string, bool>
-            {
-                [span1.SpanId.ToString()] = true,
-                [span2.SpanId.ToString()] = false
-            });
+            var samplerWithRealIds = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>
+                {
+                    [span1.SpanId.ToString()] = true,
+                    [span2.SpanId.ToString()] = false
+                },
+                attributesToAdd: new Dictionary<string, object> { ["samplingRatio"] = 2 }
+            );
 
             // Act
             var sampledActivities = SampleSpans.SampleActivities(activities, samplerWithRealIds);
@@ -124,10 +47,11 @@ namespace LaunchDarkly.Observability.Test
         public void SampleActivities_ShouldRemoveChildrenOfSpansThatAreNotSampled()
         {
             // Arrange - Create span hierarchy with parent -> child -> grandchild
-            var parentActivity = CreateMockActivity("parent");
-            var childActivity = CreateMockActivity("child", parentActivity.SpanId.ToString());
-            var grandchildActivity = CreateMockActivity("grandchild", childActivity.SpanId.ToString());
-            var rootActivity = CreateMockActivity("root");
+            var parentActivity = TestActivityHelper.CreateTestActivity("parent");
+            var childActivity = TestActivityHelper.CreateTestActivity("child", parentActivity.SpanId.ToString());
+            var grandchildActivity =
+                TestActivityHelper.CreateTestActivity("grandchild", childActivity.SpanId.ToString());
+            var rootActivity = TestActivityHelper.CreateTestActivity("root");
 
             var activities = new List<Activity>
             {
@@ -137,13 +61,15 @@ namespace LaunchDarkly.Observability.Test
                 rootActivity
             };
 
-            var mockSampler = new MockSampler(new Dictionary<string, bool>
-            {
-                [parentActivity.SpanId.ToString()] = false, // Parent not sampled
-                [childActivity.SpanId.ToString()] = true, // Child would be sampled but parent isn't
-                [grandchildActivity.SpanId.ToString()] = true, // Grandchild would be sampled but parent isn't
-                [rootActivity.SpanId.ToString()] = true // Root sampled
-            });
+            var mockSampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>
+                {
+                    [parentActivity.SpanId.ToString()] = false, // Parent not sampled
+                    [childActivity.SpanId.ToString()] = true, // Child would be sampled but parent isn't
+                    [grandchildActivity.SpanId.ToString()] = true, // Grandchild would be sampled but parent isn't
+                    [rootActivity.SpanId.ToString()] = true // Root sampled
+                }
+            );
 
             // Act
             var sampledActivities = SampleSpans.SampleActivities(activities, mockSampler);
@@ -157,15 +83,15 @@ namespace LaunchDarkly.Observability.Test
         public void SampleActivities_ShouldNotApplySamplingWhenSamplingIsDisabled()
         {
             // Arrange
-            var mockSampler = new MockSampler(
-                new Dictionary<string, bool>(), // Empty results
+            var mockSampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>(), // Empty results
                 enabled: false // Sampling disabled
             );
 
             var activities = new List<Activity>
             {
-                CreateMockActivity("span-1"),
-                CreateMockActivity("span-2")
+                TestActivityHelper.CreateTestActivity("span-1"),
+                TestActivityHelper.CreateTestActivity("span-2")
             };
 
             // Act
@@ -182,15 +108,18 @@ namespace LaunchDarkly.Observability.Test
             // Arrange
             var activities = new List<Activity>
             {
-                CreateMockActivity("span-1"),
-                CreateMockActivity("span-2")
+                TestActivityHelper.CreateTestActivity("span-1"),
+                TestActivityHelper.CreateTestActivity("span-2")
             };
 
-            var mockSampler = new MockSampler(new Dictionary<string, bool>
-            {
-                [activities[0].SpanId.ToString()] = true,
-                [activities[1].SpanId.ToString()] = true
-            });
+            var mockSampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>
+                {
+                    [activities[0].SpanId.ToString()] = true,
+                    [activities[1].SpanId.ToString()] = true
+                },
+                attributesToAdd: new Dictionary<string, object> { ["samplingRatio"] = 2 }
+            );
 
             // Act
             var sampledActivities = SampleSpans.SampleActivities(activities, mockSampler);
--- sdk/@launchdarkly/observability-dotnet/test/LaunchDarkly.Observability.Tests/SamplingTraceExporterTests.cs
+++ sdk/@launchdarkly/observability-dotnet/test/LaunchDarkly.Observability.Tests/SamplingTraceExporterTests.cs
@@ -11,85 +11,19 @@ namespace LaunchDarkly.Observability.Test
     [TestFixture]
     public class SamplingTraceExporterTests
     {
-        #region Helper Classes
-
-        /// <summary>
-        /// Mock sampler for testing
-        /// </summary>
-        private class MockSampler : IExportSampler
-        {
-            private readonly Dictionary<string, bool> _sampleResults;
-            private readonly bool _enabled;
-
-            public MockSampler(Dictionary<string, bool> sampleResults, bool enabled = true)
-            {
-                _sampleResults = sampleResults;
-                _enabled = enabled;
-            }
-
-            public void SetConfig(SamplingConfig config) { }
-
-            public SamplingResult SampleSpan(Activity span)
-            {
-                var spanId = span.SpanId.ToString();
-                var shouldSample = _sampleResults.ContainsKey(spanId) ? _sampleResults[spanId] : true;
-
-                return new SamplingResult
-                {
-                    Sample = shouldSample,
-                    Attributes = shouldSample ? new Dictionary<string, object> { ["test.sampled"] = true } : new Dictionary<string, object>()
-                };
-            }
-
-            public SamplingResult SampleLog(LogRecord record)
-            {
-                return new SamplingResult { Sample = true };
-            }
-
-            public bool IsSamplingEnabled()
-            {
-                return _enabled;
-            }
-        }
-
-        /// <summary>
-        /// Helper to create test activities
-        /// </summary>
-        private static Activity CreateTestActivity(string name, string parentSpanId = null)
-        {
-            var activity = new Activity(name);
-            activity.Start();
-            activity.DisplayName = name;
-
-            if (!string.IsNullOrEmpty(parentSpanId))
-            {
-                var traceId = ActivityTraceId.CreateRandom();
-                var parentSpan = ActivitySpanId.CreateFromString(parentSpanId.PadRight(16, '0'));
-                var parentContext = new ActivityContext(traceId, parentSpan, ActivityTraceFlags.Recorded);
-                
-                activity.Stop();
-                activity = new Activity(name);
-                activity.SetParentId(parentContext.TraceId, parentContext.SpanId, parentContext.TraceFlags);
-                activity.Start();
-                activity.DisplayName = name;
-            }
-
-            activity.Stop();
-            return activity;
-        }
-
-        #endregion
-
         [Test]
         public void SampleActivities_WhenSamplingDisabled_ShouldReturnAllActivities()
         {
             // Arrange
-            var sampler = new MockSampler(new Dictionary<string, bool>(), enabled: false);
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>(),
+                enabled: false
+            );
 
             var activities = new[]
             {
-                CreateTestActivity("span1"),
-                CreateTestActivity("span2")
+                TestActivityHelper.CreateTestActivity("span1"),
+                TestActivityHelper.CreateTestActivity("span2")
             };
 
             // Act
@@ -106,17 +40,20 @@ namespace LaunchDarkly.Observability.Test
             // Arrange
             var activities = new[]
             {
-                CreateTestActivity("span1"),
-                CreateTestActivity("span2"),
-                CreateTestActivity("span3")
+                TestActivityHelper.CreateTestActivity("span1"),
+                TestActivityHelper.CreateTestActivity("span2"),
+                TestActivityHelper.CreateTestActivity("span3")
             };
 
-            var sampler = new MockSampler(new Dictionary<string, bool>
-            {
-                [activities[0].SpanId.ToString()] = true,  // Include
-                [activities[1].SpanId.ToString()] = false, // Exclude
-                [activities[2].SpanId.ToString()] = true   // Include
-            });
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>
+                {
+                    [activities[0].SpanId.ToString()] = true, // Include
+                    [activities[1].SpanId.ToString()] = false, // Exclude
+                    [activities[2].SpanId.ToString()] = true // Include
+                },
+                attributesToAdd: new Dictionary<string, object> { ["test.sampled"] = true }
+            );
 
             // Act
             var sampledActivities = SampleSpans.SampleActivities(activities, sampler);
@@ -132,18 +69,21 @@ namespace LaunchDarkly.Observability.Test
         public void SampleActivities_WhenParentSpanIsFilteredOut_ShouldAlsoFilterOutChildren()
         {
             // Arrange
-            var parentActivity = CreateTestActivity("parent");
-            var childActivity = CreateTestActivity("child", parentActivity.SpanId.ToString());
-            var independentActivity = CreateTestActivity("independent");
+            var parentActivity = TestActivityHelper.CreateTestActivity("parent");
+            var childActivity = TestActivityHelper.CreateTestActivity("child", parentActivity.SpanId.ToString());
+            var independentActivity = TestActivityHelper.CreateTestActivity("independent");
 
             var activities = new[] { parentActivity, childActivity, independentActivity };
 
-            var sampler = new MockSampler(new Dictionary<string, bool>
-            {
-                [parentActivity.SpanId.ToString()] = false,      // Parent filtered out
-                [childActivity.SpanId.ToString()] = true,        // Child would be included but parent isn't
-                [independentActivity.SpanId.ToString()] = true   // Independent span included
-            });
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>
+                {
+                    [parentActivity.SpanId.ToString()] = false, // Parent filtered out
+                    [childActivity.SpanId.ToString()] = true, // Child would be included but parent isn't
+                    [independentActivity.SpanId.ToString()] = true // Independent span included
+                },
+                attributesToAdd: new Dictionary<string, object> { ["test.sampled"] = true }
+            );
 
             // Act
             var sampledActivities = SampleSpans.SampleActivities(activities, sampler);
@@ -159,15 +99,17 @@ namespace LaunchDarkly.Observability.Test
             // Arrange
             var activities = new[]
             {
-                CreateTestActivity("span1"),
-                CreateTestActivity("span2")
+                TestActivityHelper.CreateTestActivity("span1"),
+                TestActivityHelper.CreateTestActivity("span2")
             };
 
-            var sampler = new MockSampler(new Dictionary<string, bool>
-            {
-                [activities[0].SpanId.ToString()] = false,
-                [activities[1].SpanId.ToString()] = false
-            });
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>
+                {
+                    [activities[0].SpanId.ToString()] = false,
+                    [activities[1].SpanId.ToString()] = false
+                }
+            );
 
             // Act
             var sampledActivities = SampleSpans.SampleActivities(activities, sampler);
@@ -182,20 +124,24 @@ namespace LaunchDarkly.Observability.Test
             // Arrange
             var activities = new[]
             {
-                CreateTestActivity("span1")
+                TestActivityHelper.CreateTestActivity("span1")
             };
 
-            var sampler = new MockSampler(new Dictionary<string, bool>
-            {
-                [activities[0].SpanId.ToString()] = true
-            });
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                spanSampleResults: new Dictionary<string, bool>
+                {
+                    [activities[0].SpanId.ToString()] = true
+                },
+                attributesToAdd: new Dictionary<string, object> { ["test.sampled"] = true }
+            );
 
             // Act
             var sampledActivities = SampleSpans.SampleActivities(activities, sampler);
 
             // Assert
             Assert.That(sampledActivities.Count, Is.EqualTo(1));
-            Assert.That(sampledActivities[0].TagObjects.Any(t => t.Key == "test.sampled" && t.Value.Equals(true)), Is.True);
+            Assert.That(sampledActivities[0].TagObjects.Any(t => t.Key == "test.sampled" && t.Value.Equals(true)),
+                Is.True);
         }
     }
 }
