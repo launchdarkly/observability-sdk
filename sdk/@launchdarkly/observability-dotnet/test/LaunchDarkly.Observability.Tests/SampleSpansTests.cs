2431886bb7d0d02fa51ceefeccfaa3f761db2bea
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-08-15 08:32:22 -0700

    feat: Add export/processor samplers. (#178)
    ## Summary
    
    Adds, but does not yet use, samplers.
    
    ## How did you test this change?
    
    Unit tests.
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->

---
+++ sdk/@launchdarkly/observability-dotnet/test/LaunchDarkly.Observability.Tests/SampleSpansTests.cs
@@ -0,0 +1,133 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using LaunchDarkly.Observability.Sampling;
+using NUnit.Framework;
+
+namespace LaunchDarkly.Observability.Test
+{
+    [TestFixture]
+    public class SampleSpansTests
+    {
+        [Test]
+        public void SampleActivities_ShouldRemoveSpansThatAreNotSampled()
+        {
+            var activities = new List<Activity>
+            {
+                TestActivityHelper.CreateTestActivity("span-1"), // Root span - sampled
+                TestActivityHelper.CreateTestActivity("span-2") // Root span - not sampled
+            };
+
+            var span1 = activities[0];
+            var span2 = activities[1];
+
+            // We need to mock the span IDs in our sampler
+            var samplerWithRealIds = TestSamplerHelper.CreateMockSampler(
+                new Dictionary<string, bool>
+                {
+                    [span1.SpanId.ToString()] = true,
+                    [span2.SpanId.ToString()] = false
+                },
+                attributesToAdd: new Dictionary<string, object> { ["samplingRatio"] = 2 }
+            );
+
+            // Act
+            var sampledActivities = SampleSpans.SampleActivities(activities, samplerWithRealIds);
+
+            // Assert
+            Assert.That(sampledActivities.Count, Is.EqualTo(1));
+            Assert.That(sampledActivities[0].DisplayName, Is.EqualTo("span-1"));
+            Assert.That(sampledActivities[0].TagObjects.Any(t => t.Key == "samplingRatio" && t.Value.Equals(2)),
+                Is.True);
+        }
+
+        [Test]
+        public void SampleActivities_ShouldRemoveChildrenOfSpansThatAreNotSampled()
+        {
+            // Arrange - Create span hierarchy with parent -> child -> grandchild
+            var parentActivity = TestActivityHelper.CreateTestActivity("parent");
+            var childActivity = TestActivityHelper.CreateTestActivity("child", parentActivity.SpanId.ToString());
+            var grandchildActivity =
+                TestActivityHelper.CreateTestActivity("grandchild", childActivity.SpanId.ToString());
+            var rootActivity = TestActivityHelper.CreateTestActivity("root");
+
+            var activities = new List<Activity>
+            {
+                parentActivity,
+                childActivity,
+                grandchildActivity,
+                rootActivity
+            };
+
+            var mockSampler = TestSamplerHelper.CreateMockSampler(
+                new Dictionary<string, bool>
+                {
+                    [parentActivity.SpanId.ToString()] = false, // Parent not sampled
+                    [childActivity.SpanId.ToString()] = true, // Child would be sampled but parent isn't
+                    [grandchildActivity.SpanId.ToString()] = true, // Grandchild would be sampled but parent isn't
+                    [rootActivity.SpanId.ToString()] = true // Root sampled
+                }
+            );
+
+            // Act
+            var sampledActivities = SampleSpans.SampleActivities(activities, mockSampler);
+
+            // Assert
+            Assert.That(sampledActivities.Count, Is.EqualTo(1));
+            Assert.That(sampledActivities[0].DisplayName, Is.EqualTo("root"));
+        }
+
+        [Test]
+        public void SampleActivities_ShouldNotApplySamplingWhenSamplingIsDisabled()
+        {
+            // Arrange
+            var mockSampler = TestSamplerHelper.CreateMockSampler(
+                new Dictionary<string, bool>(), // Empty results
+                enabled: false // Sampling disabled
+            );
+
+            var activities = new List<Activity>
+            {
+                TestActivityHelper.CreateTestActivity("span-1"),
+                TestActivityHelper.CreateTestActivity("span-2")
+            };
+
+            // Act
+            var sampledActivities = SampleSpans.SampleActivities(activities, mockSampler);
+
+            // Assert
+            Assert.That(sampledActivities.Count, Is.EqualTo(2));
+            Assert.That(sampledActivities, Is.EqualTo(activities));
+        }
+
+        [Test]
+        public void SampleActivities_ShouldApplySamplingAttributesToSampledSpans()
+        {
+            // Arrange
+            var activities = new List<Activity>
+            {
+                TestActivityHelper.CreateTestActivity("span-1"),
+                TestActivityHelper.CreateTestActivity("span-2")
+            };
+
+            var mockSampler = TestSamplerHelper.CreateMockSampler(
+                new Dictionary<string, bool>
+                {
+                    [activities[0].SpanId.ToString()] = true,
+                    [activities[1].SpanId.ToString()] = true
+                },
+                attributesToAdd: new Dictionary<string, object> { ["samplingRatio"] = 2 }
+            );
+
+            // Act
+            var sampledActivities = SampleSpans.SampleActivities(activities, mockSampler);
+
+            // Assert
+            Assert.That(sampledActivities.Count, Is.EqualTo(2));
+            Assert.That(sampledActivities[0].TagObjects.Any(t => t.Key == "samplingRatio" && t.Value.Equals(2)),
+                Is.True);
+            Assert.That(sampledActivities[1].TagObjects.Any(t => t.Key == "samplingRatio" && t.Value.Equals(2)),
+                Is.True);
+        }
+    }
+}
---
+++ sdk/@launchdarkly/observability-dotnet/test/LaunchDarkly.Observability.Tests/SamplingLogProcessorTests.cs
@@ -0,0 +1,202 @@
+using System.Collections.Generic;
+using System.Linq;
+using LaunchDarkly.Observability.Otel;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+
+namespace LaunchDarkly.Observability.Test
+{
+    [TestFixture]
+    public class SamplingLogProcessorTests
+    {
+        [Test]
+        public void OnEnd_WhenSamplerReturnsFalse_ShouldNotModifyAttributes()
+        {
+            var sampler = TestSamplerHelper.CreateMockSampler(shouldSampleLogs: false);
+            var processor = new SamplingLogProcessor(sampler);
+            var logRecord = LogRecordHelper.CreateTestLogRecord(LogLevel.Information, "Test message");
+            var originalAttributes = logRecord.Attributes?.ToList();
+
+            processor.OnEnd(logRecord);
+
+            var finalAttributes = logRecord.Attributes?.ToList();
+
+            if (originalAttributes == null)
+                Assert.That(finalAttributes, Is.Null.Or.Empty);
+            else
+                Assert.That(finalAttributes?.Count, Is.EqualTo(originalAttributes.Count));
+        }
+
+        [Test]
+        public void OnEnd_WhenSamplerReturnsTrue_WithNoAttributes_ShouldNotModifyRecord()
+        {
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                shouldSampleLogs: true,
+                attributesToAdd: new Dictionary<string, object>()
+            );
+            var processor = new SamplingLogProcessor(sampler);
+            var logRecord = LogRecordHelper.CreateTestLogRecord(LogLevel.Information, "Test message");
+            var originalAttributes = logRecord.Attributes?.ToList();
+
+            processor.OnEnd(logRecord);
+
+            var finalAttributes = logRecord.Attributes?.ToList();
+
+            if (originalAttributes == null)
+                Assert.That(finalAttributes, Is.Null.Or.Empty);
+            else
+                Assert.That(finalAttributes?.Count, Is.EqualTo(originalAttributes.Count));
+        }
+
+        [Test]
+        public void OnEnd_WhenSamplerAddsAttributes_ShouldMergeWithExistingAttributes()
+        {
+            var samplingAttributes = new Dictionary<string, object>
+            {
+                ["sampling.ratio"] = 0.5,
+                ["sampler.type"] = "custom"
+            };
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                shouldSampleLogs: true,
+                attributesToAdd: samplingAttributes
+            );
+            var processor = new SamplingLogProcessor(sampler);
+
+            var existingAttributes = new Dictionary<string, object>
+            {
+                ["existing.key"] = "existing.value",
+                ["another.key"] = 42
+            };
+            var logRecord = LogRecordHelper.CreateTestLogRecord(LogLevel.Information, "Test message with attributes",
+                existingAttributes);
+
+            processor.OnEnd(logRecord);
+
+            Assert.That(logRecord.Attributes, Is.Not.Null);
+
+            var attributesList = logRecord.Attributes.ToList();
+
+            Assert.Multiple(() =>
+            {
+                // Check that sampling attributes were added
+                Assert.That(attributesList.Any(kvp =>
+                {
+                    Assert.That(kvp.Value, Is.Not.Null);
+                    return kvp.Key == "sampling.ratio" && kvp.Value.Equals(0.5);
+                }), Is.True);
+                Assert.That(attributesList.Any(kvp =>
+                {
+                    Assert.That(kvp.Value, Is.Not.Null);
+                    return kvp.Key == "sampler.type" && kvp.Value.Equals("custom");
+                }), Is.True);
+
+                // Check that existing attributes are preserved
+                Assert.That(attributesList.Any(kvp =>
+                    {
+                        Assert.That(kvp.Value, Is.Not.Null);
+                        return kvp.Key == "existing.key" && kvp.Value.Equals("existing.value");
+                    }),
+                    Is.True);
+                Assert.That(attributesList.Any(kvp =>
+                {
+                    Assert.That(kvp.Value, Is.Not.Null);
+                    return kvp.Key == "another.key" && kvp.Value.Equals(42);
+                }), Is.True);
+            });
+        }
+
+        [Test]
+        public void OnEnd_WhenLogHasNoExistingAttributes_ShouldAddSamplingAttributes()
+        {
+            var samplingAttributes = new Dictionary<string, object>
+            {
+                ["sampling.ratio"] = 1.0,
+                ["sampler.enabled"] = true
+            };
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                shouldSampleLogs: true,
+                attributesToAdd: samplingAttributes
+            );
+            var processor = new SamplingLogProcessor(sampler);
+            var logRecord =
+                LogRecordHelper.CreateTestLogRecord(LogLevel.Information, "Test message without attributes");
+
+            processor.OnEnd(logRecord);
+
+            Assert.That(logRecord.Attributes, Is.Not.Null);
+
+            var attributesList = logRecord.Attributes.ToList();
+
+            Assert.Multiple(() =>
+            {
+                // Check that sampling attributes were added
+                Assert.That(attributesList.Any(kvp =>
+                {
+                    Assert.That(kvp.Value, Is.Not.Null);
+                    return kvp.Key == "sampling.ratio" && kvp.Value.Equals(1.0);
+                }), Is.True);
+                Assert.That(attributesList.Any(kvp =>
+                {
+                    Assert.That(kvp.Value, Is.Not.Null);
+                    return kvp.Key == "sampler.enabled" && kvp.Value.Equals(true);
+                }), Is.True);
+            });
+        }
+
+        [Test]
+        public void OnEnd_WhenSamplingRatioAttributeIsAdded_ShouldBeIncludedInLogAttributes()
+        {
+            var samplingAttributes = new Dictionary<string, object>
+            {
+                ["sampling.ratio"] = 0.25
+            };
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                shouldSampleLogs: true,
+                attributesToAdd: samplingAttributes
+            );
+            var processor = new SamplingLogProcessor(sampler);
+            var logRecord =
+                LogRecordHelper.CreateTestLogRecord(LogLevel.Information, "Test message for sampling ratio");
+
+            processor.OnEnd(logRecord);
+
+            Assert.That(logRecord.Attributes, Is.Not.Null);
+
+            var attributesList = logRecord.Attributes.ToList();
+            Assert.That(attributesList.Any(kvp =>
+            {
+                Assert.That(kvp.Value, Is.Not.Null);
+                return kvp.Key == "sampling.ratio" && kvp.Value.Equals(0.25);
+            }), Is.True);
+        }
+
+        [Test]
+        public void OnEnd_WhenSamplerHasEmptyAttributes_ShouldNotAddAttributes()
+        {
+            var sampler = TestSamplerHelper.CreateMockSampler(
+                shouldSampleLogs: true,
+                attributesToAdd: new Dictionary<string, object>()
+            );
+            var processor = new SamplingLogProcessor(sampler);
+
+            var existingAttributes = new Dictionary<string, object>
+            {
+                ["original.key"] = "original.value"
+            };
+            var logRecord =
+                LogRecordHelper.CreateTestLogRecord(LogLevel.Information, "Test message", existingAttributes);
+            var originalAttributesList = logRecord.Attributes?.ToList() ?? new List<KeyValuePair<string, object>>();
+
+            processor.OnEnd(logRecord);
+
+            var finalAttributesList = logRecord.Attributes?.ToList() ?? new List<KeyValuePair<string, object>>();
+            Assert.That(finalAttributesList, Has.Count.EqualTo(originalAttributesList.Count));
+            Assert.That(finalAttributesList.Any(kvp =>
+                {
+                    Assert.That(kvp.Value, Is.Not.Null);
+                    return kvp.Key == "original.key" && kvp.Value.Equals("original.value");
+                }),
+                Is.True);
+        }
+    }
+}
