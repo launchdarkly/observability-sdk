db54734836eece6b0ad34b3580968a07f9eb6993
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-08-21 13:10:42 -0700

    Move to common options.

--- sdk/highlight-run/src/client/types/types.ts
+++ sdk/highlight-run/src/client/types/types.ts
@@ -134,6 +134,26 @@ export type CommonOptions = {
 	 * Otherwise, the current session logic will be used to generate a sessionSecureID.
 	 */
 	sessionKey?: string
+
+	/**
+	 * A function that returns a friendly name for a given context.
+	 * This name will be used to identify the session in the observability UI.
+	 * ```ts
+	 * contextFriendlyName: (context: LDContext) => {
+	 *   if(context.kind === 'multi' && context.user?.email) {
+	 *     return context.user.email;
+	 *   } else if(context.kind === 'user') {
+	 *     return context.key;
+	 *   }
+	 *   // Use the default identifier for contexts which don't contain a user.
+	 *   return undefined;
+	 * }
+	 * ```
+	 * @param context The context to get a friendly name for.
+	 * @returns The friendly name for the context, or undefined to use the
+	 * default identifier.
+	 */
+	contextFriendlyName?: (context: LDContext) => string | undefined
 }
 
 export type HighlightOptions = CommonOptions & {
