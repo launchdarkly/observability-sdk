b2151092fa5ccedabf2111b1c63041c2f8bfaefb
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-09-02 15:14:33 -0700

    feat: Add support for non-core ASP.Net. (#199)
    ## Summary
    
    Adds support for ASP.Net on .Net Framework.
    netstandard2.0 and dotnet 8.0 support ASP.Net core.
    
    Code that is common between the two is shared, code and dependencies
    that are not common are conditionally included.
    
    ## How did you test this change?
    
    Manual testing.
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->

---
+++ sdk/@launchdarkly/observability-dotnet/src/LaunchDarkly.Observability/Asp/Legacy/OpenTelemetryConfig.cs
@@ -0,0 +1,100 @@
+using LaunchDarkly.Logging;
+using LaunchDarkly.Observability.Otel;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+
+// ReSharper disable once CheckNamespace
+namespace LaunchDarkly.Observability
+{
+    public static class OpenTelemetry
+    {
+        private static TracerProvider _tracerProvider;
+        private static MeterProvider _meterProvider;
+        private static readonly object ProviderLock = new object();
+        private static ILoggerFactory _loggerFactory;
+
+        /// <summary>
+        /// Extension method which adds LaunchDarkly logging to a logging factory.
+        ///
+        /// <code>
+        /// using (var factory = LoggerFactory.Create(builder => { builder.AddLaunchDarklyLogging(config); })) {
+        ///   // Use factory to get logger which uses LaunchDarkly loging.
+        /// }
+        /// </code>
+        /// </summary>
+        /// <param name="loggingBuilder">the logging builder for the factory</param>
+        /// <param name="config">the LaunchDarkly observability configuration</param>
+        public static void AddLaunchDarklyLogging(this ILoggingBuilder loggingBuilder, ObservabilityConfig config)
+        {
+            loggingBuilder.AddOpenTelemetry(options =>
+            {
+                options.SetResourceBuilder(CommonOtelOptions.GetResourceBuilder(config))
+                    .AddProcessor(new SamplingLogProcessor(CommonOtelOptions.GetSampler(config)))
+                    .AddOtlpExporter(exportOptions => { exportOptions.WithCommonLaunchDarklyLoggingExport(config); });
+
+                config.ExtendedLoggerConfiguration?.Invoke(options);
+            });
+        }
+
+        /// <summary>
+        /// Configure LaunchDarkly observability. In typical usage, the ObservabilityPlugin should be used instead.
+        /// <para>
+        /// This method is for advanced use-cases where the LaunchDarkly client needs to be initialized later than the
+        /// telemetry implementation.
+        /// </para>
+        /// <para>
+        /// If this method is used, then the <see cref="ObservabilityPlugin"/> should be instantiated using
+        /// <see cref="ObservabilityPlugin.ForExistingServices"/> method.
+        /// </para>
+        /// </summary>
+        /// <param name="config">configuration for LaunchDarkly Observability</param>
+        /// <param name="debugLogger">an optional debug logger</param>
+        public static void Register(ObservabilityConfig config, Logger debugLogger = null)
+        {
+            lock (ProviderLock)
+            {
+                // If the providers are set, then the implementation has already been configured.
+                if (_tracerProvider != null)
+                {
+                    return;
+                }
+
+                var resourceBuilder = CommonOtelOptions.GetResourceBuilder(config);
+                var sampler = CommonOtelOptions.GetSampler(config);
+
+                _tracerProvider = global::OpenTelemetry.Sdk.CreateTracerProviderBuilder()
+                    .WithCommonLaunchDarklyConfig(config, resourceBuilder, sampler)
+                    .AddAspNetInstrumentation()
+                    .Build();
+
+                _meterProvider = global::OpenTelemetry.Sdk.CreateMeterProviderBuilder()
+                    .WithCommonLaunchDarklyConfig(config, resourceBuilder)
+                    .AddAspNetInstrumentation()
+                    .Build();
+
+                _loggerFactory = LoggerFactory.Create(builder => { builder.AddLaunchDarklyLogging(config); });
+                var logger = _loggerFactory.CreateLogger<ObservabilityPlugin>();
+                Observe.Initialize(config, logger);
+            }
+        }
+
+        /// <summary>
+        /// Shutdown the underlying telemetry.
+        /// </summary>
+        public static void Shutdown()
+        {
+            lock (ProviderLock)
+            {
+                _tracerProvider?.Dispose();
+                _meterProvider?.Dispose();
+                _loggerFactory?.Dispose();
+                _tracerProvider = null;
+                _meterProvider = null;
+                _loggerFactory = null;
+            }
+        }
+    }
+}
