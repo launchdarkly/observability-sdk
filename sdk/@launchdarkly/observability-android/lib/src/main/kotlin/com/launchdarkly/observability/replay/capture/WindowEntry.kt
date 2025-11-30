343690e898492c25a60c05d959dde2e5983f1a4a
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-11-30 15:09:12 -0800

    use Canvas for certain types of windows

--- sdk/@launchdarkly/observability-android/lib/src/main/kotlin/com/launchdarkly/observability/replay/capture/WindowEntry.kt
+++ sdk/@launchdarkly/observability-android/lib/src/main/kotlin/com/launchdarkly/observability/replay/capture/WindowEntry.kt
@@ -1,7 +1,11 @@
 package com.launchdarkly.observability.replay.capture
 
 import android.graphics.Rect
+import android.os.Build
+import android.text.Layout
 import android.view.View
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams.TYPE_APPLICATION_STARTING
 
 
 enum class WindowType {
@@ -13,7 +17,7 @@ enum class WindowType {
 data class WindowEntry(
     val rootView: View,
     var type: WindowType,
-    val wmType: Int,
+    val layoutParams: WindowManager.LayoutParams?,
     val width: Int,
     val height: Int,
     val screenLeft: Int,
@@ -21,6 +25,26 @@ data class WindowEntry(
 ) {
     fun rect(): Rect {
         return Rect(0, 0, width, height)
+    }
+
+    fun isPixelCopyCandidate(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return false
+        }
+        if (type != WindowType.ACTIVITY) {
+            return false
+        }
+
+        if (layoutParams?.type == TYPE_APPLICATION_STARTING) { // Starting/Splash screen
+            return false
+        }
+
+        if (((layoutParams?.flags ?: 0) and WindowManager.LayoutParams.FLAG_SECURE) != 0) {
+            // Secure window
+            return false
+        }
+
+        return true
     }
 }
 
