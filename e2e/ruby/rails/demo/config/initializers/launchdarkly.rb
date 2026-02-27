971d53763b145792ba476817b1096ce5c197acc0
ccschmitz <christopher.schmitz@launchdarkly.com>
2026-02-27 12:16:31 -0600

    Update launchdarkly.rb

--- e2e/ruby/rails/demo/config/initializers/launchdarkly.rb
+++ e2e/ruby/rails/demo/config/initializers/launchdarkly.rb
@@ -13,10 +13,5 @@ sdk_key = ENV.fetch('LAUNCHDARKLY_SDK_KEY') do
 end
 
 config = LaunchDarkly::Config.new(plugins: [observability_plugin])
-
-if sdk_key
-  Rails.configuration.ld_client = LaunchDarkly::LDClient.new(sdk_key, config)
-  at_exit { Rails.configuration.ld_client&.close }
-else
-  Rails.configuration.ld_client = nil
-end
+Rails.configuration.ld_client = LaunchDarkly::LDClient.new(sdk_key, config)
+at_exit { Rails.configuration.ld_client&.close }
