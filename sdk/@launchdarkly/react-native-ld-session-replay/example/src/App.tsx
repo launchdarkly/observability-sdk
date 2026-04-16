cc05d25e0db66fba2c8933f9e6adcc8e61d1ae50
Bee Klimt <bklimt@launchdarkly.com>
2026-04-16 13:50:46 -0700

    style: run prettier

--- sdk/@launchdarkly/react-native-ld-session-replay/example/src/App.tsx
+++ sdk/@launchdarkly/react-native-ld-session-replay/example/src/App.tsx
@@ -1,12 +1,12 @@
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native';
 import {
   ReactNativeLDClient,
   LDProvider,
   AutoEnvAttributes,
-} from '@launchdarkly/react-native-client-sdk'
-import { useEffect } from 'react'
-import { createSessionReplayPlugin } from '@launchdarkly/session-replay-react-native'
-import DialogsScreen from './DialogsScreen'
+} from '@launchdarkly/react-native-client-sdk';
+import { useEffect } from 'react';
+import { createSessionReplayPlugin } from '@launchdarkly/session-replay-react-native';
+import DialogsScreen from './DialogsScreen';
 
 const plugin = createSessionReplayPlugin({
   isEnabled: true,
@@ -16,22 +16,22 @@ const plugin = createSessionReplayPlugin({
   maskImages: true,
   maskAccessibilityIdentifiers: ['password', 'ssn'],
   minimumAlpha: 0.05,
-})
+});
 
 // Replace with your LaunchDarkly mobile key.
 // You can also set the LAUNCHDARKLY_MOBILE_KEY environment variable.
 const MOBILE_KEY =
-  process.env.LAUNCHDARKLY_MOBILE_KEY || 'YOUR_LAUNCHDARKLY_MOBILE_KEY_HERE'
+  process.env.LAUNCHDARKLY_MOBILE_KEY || 'YOUR_LAUNCHDARKLY_MOBILE_KEY_HERE';
 
 const client = new ReactNativeLDClient(MOBILE_KEY, AutoEnvAttributes.Enabled, {
   plugins: [plugin],
-})
-const context = { kind: 'user', key: 'user-key-123abc' }
+});
+const context = { kind: 'user', key: 'user-key-123abc' };
 
 export default function App() {
   useEffect(() => {
-    client.identify(context).catch((e: unknown) => console.log(e))
-  }, [])
+    client.identify(context).catch((e: unknown) => console.log(e));
+  }, []);
 
   return (
     <LDProvider client={client}>
@@ -39,5 +39,5 @@ export default function App() {
         <DialogsScreen />
       </SafeAreaView>
     </LDProvider>
-  )
+  );
 }
